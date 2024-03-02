614c8a66e13cd1188f9d80eeab2411ef9897310c
Seth Horsley <isethi@me.com>
2024-03-02 23:57:50 +0100

    ✨ init ✨

---
+++ lib/clsx.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clsx/version"
+
+module Clsx
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/clsx/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Clsx
+  VERSION = "0.1.0"
+end
